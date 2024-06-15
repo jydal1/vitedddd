dda4c13dcd1bde66b7e05ff4f51e494329892d3b
aros <jerrybosche@gmail.com>
2024-06-15 11:48:07 +0300

    fix ref

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -7,134 +7,127 @@ import Tasks from './Task';
 import BottomMenu from './BMenu';
 
 interface FloatingElement {
-    id: string;
-    x: number;
-    y: number;
-    text: string;
+id: string;
+x: number;
+y: number;
+text: string;
 }
 
 function App() {
-    const [counter, setCounter] = useState<number>(0);
-    const [currentView, setCurrentView] = useState<string>('home');
-    const [floatingElements, setFloatingElements] = useState<FloatingElement[]>([]);
-    const [telegramId, setTelegramId] = useState<string | null>(null);
+const [counter, setCounter] = useState<number>(0);
+const [currentView, setCurrentView] = useState<string>('home');
+const [floatingElements, setFloatingElements] = useState<FloatingElement[]>([]);
+useEffect(() => {
+    async function fetchCounter() {
+        const response = await fetch('https://devbackend-f7a664bc1045.herokuapp.com/counter/');
+        const data = await response.json();
+        setCounter(data.count);
+    }
+    fetchCounter();
 
-    useEffect(() => {
-        const urlParams = new URLSearchParams(window.location.search);
-        const telegram_id = urlParams.get('telegram_id');
-        if (telegram_id) {
-            localStorage.setItem('telegram_id', telegram_id);
-            setTelegramId(telegram_id);
-        } else {
-            setTelegramId(localStorage.getItem('telegram_id'));
-        }
-
-        if (telegramId) {
-            async function fetchCounter() {
-                const response = await fetch(`https://devbackend-f7a664bc1045.herokuapp.com/counter/${telegramId}`);
-                const data = await response.json();
-                setCounter(data.count);
-            }
-            fetchCounter();
-        }
-
-        if (window.Telegram && window.Telegram.WebApp) {
-            window.Telegram.WebApp.expand();
-        }
-    }, [telegramId]);
-
-    async function handleClick(e: React.MouseEvent) {
-        const clientX = e.clientX;
-        const clientY = e.clientY;
-
-        try {
-            const response = await fetch('https://devbackend-f7a664bc1045.herokuapp.com/counter/increment/', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({ user_id: telegramId, amount: 1 }), // Передаємо ідентифікатор користувача та значення, на яке інкрементуємо
-            });
-            if (!response.ok) {
-                throw new Error('Failed to increment counter');
-            }
-            const data = await response.json();
-            setCounter(data.count);
-            localStorage.setItem('Counter', data.count.toString());
-
-            const currentTarget = e.currentTarget as HTMLElement | null;
-
-            if (currentTarget) {
-                const boxRect = currentTarget.getBoundingClientRect();
-                const x = clientX - boxRect.left;
-                const y = clientY - boxRect.top;
-                addFloatingElement(x, y, '+1');
-            }
-        } catch (error) {
-            console.error(error);
-        }
+    if (window.Telegram && window.Telegram.WebApp) {
+        window.Telegram.WebApp.expand();
     }
 
-    function addFloatingElement(x: number, y: number, text: string) {
-        const id = uuidv4();
-        setFloatingElements(prevElements => [
-            ...prevElements,
-            { id, x, y, text }
-        ]);
-
-        setTimeout(() => {
-            setFloatingElements(prevElements => prevElements.filter(el => el.id !== id));
-        }, 1000);
+    const urlParams = new URLSearchParams(window.location.search);
+    const telegram_id = urlParams.get('telegram_id');
+    if (telegram_id) {
+        localStorage.setItem('telegram_id', telegram_id);
     }
+}, []);
 
-    function addToCounter(amount: number) {
-        setCounter(prevCounter => prevCounter + amount);
-    }
+async function handleClick(e: React.MouseEvent) {
+    const clientX = e.clientX;
+    const clientY = e.clientY;
 
-    function renderView() {
-        switch (currentView) {
-            case 'home':
-                return (
-                    <>
-                        <div className='hello'>
-                            <h1>👋Hello, s2</h1>
-                        </div>
-                        <div className="box" style={{ position: 'relative' }}>
-                            <span className='counter'>🦈{counter}</span>
-                            <img src={coin} alt="coin" width={312} height={312} onClick={handleClick} style={{marginTop: '60px'}}/>
-                            {floatingElements.map(el => (
-                                <span key={el.id} className='floating-text' style={{ left: el.x, top: el.y }}>
-                                    {el.text}
-                                </span>
-                            ))}
-                        </div>
-                    </>
-                );
-            case 'friends':
-                return <Friends addToCounter={addToCounter} />;
-            case 'tasks':
-                return <Tasks />;
-            default:
-                return (
-                    <>
-                        <div className='hello'>
-                            <h1>👋Hello</h1>
-                        </div>
-                        <div className="box" style={{ position: 'relative' }}>
-                            <span className='counter'>{counter}</span>
-                            <img src={coin} alt="coin" width={256} height={256} onClick={handleClick} />
-                        </div>
-                    </>
-                );
+    try {
+        const response = await fetch('https://devbackend-f7a664bc1045.herokuapp.com/counter/increment/', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ amount: 1 }), // Передаємо значення, на яке інкрементуємо
+        });
+        if (!response.ok) {
+            throw new Error('Failed to increment counter');
         }
-    }
+        const data = await response.json();
+        setCounter(data.count);
+        localStorage.setItem('Counter', data.count.toString());
 
-    return (
-        <>
-            {renderView()}
-            <BottomMenu setCurrentView={setCurrentView} />
-        </>
-    );
+        const currentTarget = e.currentTarget as HTMLElement | null;
+
+        if (currentTarget) {
+            const boxRect = currentTarget.getBoundingClientRect();
+            const x = clientX - boxRect.left;
+            const y = clientY - boxRect.top;
+            addFloatingElement(x, y, '+1');
+        }
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+function addFloatingElement(x: number, y: number, text: string) {
+    const id = uuidv4();
+    setFloatingElements(prevElements => [
+        ...prevElements,
+        { id, x, y, text }
+    ]);
+
+    setTimeout(() => {
+        setFloatingElements(prevElements => prevElements.filter(el => el.id !== id));
+    }, 1000);
+}
+
+function addToCounter(amount: number) {
+    setCounter(prevCounter => prevCounter + amount);
+}
+
+function renderView() {
+    switch (currentView) {
+        case 'home':
+            return (
+                <>
+                    <div className='hello'>
+                        <h1>👋Hello, s11</h1>
+                    </div>
+                    <div className="box" style={{ position: 'relative' }}>
+                        <span className='counter'>🦈{counter}</span>
+                        <img src={coin} alt="coin" width={312} height={312} onClick={handleClick} style={{marginTop: '60px'}}/>
+                        {floatingElements.map(el => (
+                            <span key={el.id} className='floating-text' style={{ left: el.x, top: el.y }}>
+                                {el.text}
+                            </span>
+                        ))}
+                    </div>
+                </>
+            );
+        case 'friends':
+            return <Friends addToCounter={addToCounter} />;
+        case 'tasks':
+            return <Tasks />;
+        default:
+            return (
+                <>
+                    <div className='hello'>
+                        <h1>👋Hello</h1>
+                    </div>
+                    <div className="box" style={{ position: 'relative' }}>
+                        <span className='counter'>{counter}</span>
+                        <img src={coin} alt="coin" width={256} height={256} onClick={handleClick} />
+                    </div>
+                </>
+            );
+    }
+}
+
+return (
+    <>
+        {renderView()}
+        <BottomMenu setCurrentView={setCurrentView} />
+    </>
+);
 }
 
 export default App;
